cabbb89bace372beb55695e311dd8dbb637bd7ab
Abanta6 <dalexbanta95@gmail.com>
2025-02-26 03:15:32 -0600

    Created js, and started initial conversion from guided practice

---
+++ index.js
@@ -0,0 +1,111 @@
+/**
+ * @typedef Artist
+ * @property {number} id
+ * @property {string} name
+ * @property {string} description
+ * @property {string} imageUrl
+ */
+
+// === Constants ===
+const BASE = "https://fsa-crud-2aa9294fe819.herokuapp.com/api";
+const COHORT = "/"; // Make sure to change this!
+const RESOURCE = "/artists";
+const API = BASE + COHORT;
+
+// === State ===
+let parties = [];
+let selectedParty;
+
+/** Updates state with all artists from the API */
+async function getParties() {
+  try {
+    const response = await fetch(API + "/events");
+    const result = await response.json();
+    parties = result.data;
+    render();
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+/** Updates state with a single artist from the API */
+async function getParty(id) {
+  try {
+    const response = await fetch(API + "/events/" + id);
+    const result = await response.json();
+    selectedParties = result.data;
+    render();
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+// === Components ===
+
+/** Artist name that shows more details about the artist when clicked */
+function ArtistListItem(artist) {
+  const $li = document.createElement("li");
+  $li.innerHTML = `
+    <a href="#selected">${artist.name}</a>
+  `;
+  $li.addEventListener("click", () => getArtist(artist.id));
+  return $li;
+}
+
+/** A list of names of all artists */
+function ArtistList() {
+  const $ul = document.createElement("ul");
+  $ul.classList.add("lineup");
+
+  const $artists = artists.map(ArtistListItem);
+  $ul.replaceChildren(...$artists);
+
+  return $ul;
+}
+
+/** Detailed information about the selected artist */
+function ArtistDetails() {
+  if (!selectedArtist) {
+    const $p = document.createElement("p");
+    $p.textContent = "Please select an artist to learn more.";
+    return $p;
+  }
+
+  const $artist = document.createElement("section");
+  $artist.classList.add("artist");
+  $artist.innerHTML = `
+    <h3>${selectedArtist.name} #${selectedArtist.id}</h3>
+    <figure>
+      <img alt=${selectedArtist.name} src=${selectedArtist.imageUrl} />
+    </figure>
+    <p>${selectedArtist.description}</p>
+  `;
+  return $artist;
+}
+
+// === Render ===
+function render() {
+  const $app = document.querySelector("#app");
+  $app.innerHTML = `
+    <h1>Fullstack Gala</h1>
+    <main>
+      <section>
+        <h2>Lineup</h2>
+        <ArtistList></ArtistList>
+      </section>
+      <section id="selected">
+        <h2>Artist Details</h2>
+        <ArtistDetails></ArtistDetails>
+      </section>
+    </main>
+  `;
+  $app.querySelector("ArtistList").replaceWith(ArtistList());
+  $app.querySelector("ArtistDetails").replaceWith(ArtistDetails());
+}
+
+async function init() {
+  await getArtists();
+  render();
+}
+
+init();
