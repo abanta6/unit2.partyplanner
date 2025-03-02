7003300c359a45dc0861ffba3fd90c003a482c3b
Abanta6 <dalexbanta95@gmail.com>
2025-03-01 20:02:58 -0600

    Parties listed and descripted, and CSS styled. git push

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
-/**
- * @typedef Artist
- * @property {number} id
- * @property {string} name
- * @property {string} description
- * @property {string} imageUrl
- */
-
 // === Constants ===
 const BASE = "https://fsa-crud-2aa9294fe819.herokuapp.com/api";
-const COHORT = "/"; // Make sure to change this!
-const RESOURCE = "/artists";
+const COHORT = "/2412-Banta"; // Make sure to change this!!
 const API = BASE + COHORT;
 
 // === State ===
 let parties = [];
 let selectedParty;
 
-/** Updates state with all artists from the API */
 async function getParties() {
   try {
     const response = await fetch(API + "/events");
@@ -28,12 +18,11 @@ async function getParties() {
   }
 }
 
-/** Updates state with a single artist from the API */
 async function getParty(id) {
   try {
     const response = await fetch(API + "/events/" + id);
     const result = await response.json();
-    selectedParties = result.data;
+    selectedParty = result.data;
     render();
   } catch (e) {
     console.error(e);
@@ -42,69 +31,73 @@ async function getParty(id) {
 
 // === Components ===
 
-/** Artist name that shows more details about the artist when clicked */
-function ArtistListItem(artist) {
+function PartyListItem(party) {
   const $li = document.createElement("li");
+
+  if (party.id === selectedParty?.id) {
+    $li.classList.add("selected");
+  }
+
   $li.innerHTML = `
-    <a href="#selected">${artist.name}</a>
+    <a href="#selected">${party.name}</a>
   `;
-  $li.addEventListener("click", () => getArtist(artist.id));
+  $li.addEventListener("click", () => getParty(party.id));
   return $li;
 }
 
-/** A list of names of all artists */
-function ArtistList() {
+function PartyList() {
   const $ul = document.createElement("ul");
-  $ul.classList.add("lineup");
+  $ul.classList.add("parties");
 
-  const $artists = artists.map(ArtistListItem);
-  $ul.replaceChildren(...$artists);
+  const $parties = parties.map(PartyListItem);
+  $ul.replaceChildren(...$parties);
 
   return $ul;
 }
 
 /** Detailed information about the selected artist */
-function ArtistDetails() {
-  if (!selectedArtist) {
+function SelectedParty() {
+  if (!selectedParty) {
     const $p = document.createElement("p");
-    $p.textContent = "Please select an artist to learn more.";
+    $p.textContent = "Please select a party to learn more.";
     return $p;
   }
 
-  const $artist = document.createElement("section");
-  $artist.classList.add("artist");
-  $artist.innerHTML = `
-    <h3>${selectedArtist.name} #${selectedArtist.id}</h3>
-    <figure>
-      <img alt=${selectedArtist.name} src=${selectedArtist.imageUrl} />
-    </figure>
-    <p>${selectedArtist.description}</p>
+  const $party = document.createElement("section");
+  $party.innerHTML = `
+    <h3>${selectedParty.name} #${selectedParty.id}</h3>
+      <time datetime="${selectedParty.date}">
+      ${selectedParty.date.slice(0, 10)}
+    </time>
+    <address>${selectedParty.location}</address>
+    <p>${selectedParty.description}</p>
   `;
-  return $artist;
+
+  return $party;
 }
 
 // === Render ===
 function render() {
   const $app = document.querySelector("#app");
   $app.innerHTML = `
-    <h1>Fullstack Gala</h1>
+    <h1>Party Planner</h1>
     <main>
       <section>
-        <h2>Lineup</h2>
-        <ArtistList></ArtistList>
+        <h2>Upcoming Parties</h2>
+        <PartyList></PartyList>
       </section>
       <section id="selected">
-        <h2>Artist Details</h2>
-        <ArtistDetails></ArtistDetails>
+        <h2>Party Details</h2>
+        <SelectedParty></SelectedParty>
       </section>
     </main>
   `;
-  $app.querySelector("ArtistList").replaceWith(ArtistList());
-  $app.querySelector("ArtistDetails").replaceWith(ArtistDetails());
+  $app.querySelector("PartyList").replaceWith(PartyList());
+  $app.querySelector("SelectedParty").replaceWith(SelectedParty());
 }
 
 async function init() {
-  await getArtists();
+  await getParties();
   render();
 }
 
